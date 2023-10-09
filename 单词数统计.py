1620e2317fb632b514c0afc7b42bf2d0404ae153
Rsec <119555373+Rsec-1@users.noreply.github.com>
2023-10-09 20:48:48 +0800

    Update 单词数统计.py

--- 单词数统计.py
+++ 单词数统计.py
@@ -14,13 +14,13 @@ def get_html_of(url):
     print(f'error: {e}')
     exit(1)
 
-def get_top_words((text,n=10):
+def get_top_words(text,n=10):
   all_words = re.findall(r'\w+',text.lower())
   word_count = Counter(all_words)
   return word_count.most_common(n)
 
 if __name__ == '__main__':
-  html = get_htmlof(page_url)
+  html = get_html_of(page_url)
   soup = BeautifulSoup(html,'html.parser')
   raw_text = soup.get_text()
   top_words = get_top_words(raw_text)
